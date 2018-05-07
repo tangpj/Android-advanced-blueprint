ddcf7d1939cdeda58b9791d46f91085956c93341
TPJ <514820184@qq.com>
2018-05-07 15:31:27 +0800

    完成Demo

--- todoDatabinding/app/src/main/java/tang/com/tododatabinding/login/LoginViewModel.java
+++ todoDatabinding/app/src/main/java/tang/com/tododatabinding/login/LoginViewModel.java
@@ -3,12 +3,15 @@ package tang.com.tododatabinding.login;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Intent;
 import android.databinding.ObservableField;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
 import java.util.Objects;
+
+import tang.com.tododatabinding.MainActivity;
 
 public class LoginViewModel extends ViewModel {
 
@@ -22,14 +25,19 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void login(View view){
+        String loginMsg =  "accountNum = " + Objects.requireNonNull(account.get()).getAccountNum().get()
+                + "\npassword = " + Objects.requireNonNull(account.get()).getPassword().get();
         Log.d(TAG,"正在登陆中....\n"
-                + "accountNum = " + Objects.requireNonNull(account.get()).getAccountNum().get()
-                + "\npassword = " + Objects.requireNonNull(account.get()).getPassword().get());
+               + loginMsg);
         isLoading.set(true);
             new Handler().postDelayed(() -> {
                 Log.d(TAG,"登陆成功....\n");
                 isLoading.set(false);
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                intent.putExtra("hello", loginMsg);
+                view.getContext().startActivity(intent);
             }, 2000);
+
     }
 
     public ObservableField<Boolean> getIsLoading(){
