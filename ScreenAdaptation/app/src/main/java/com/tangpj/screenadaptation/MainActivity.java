eb4937177363a291e22975c03bde46e77cce33f3
TPJ <514820184@qq.com>
2018-09-27 10:37:27 +0800

    create mipmap

--- ScreenAdaptation/app/src/main/java/com/tangpj/screenadaptation/MainActivity.java
+++ ScreenAdaptation/app/src/main/java/com/tangpj/screenadaptation/MainActivity.java
@@ -2,12 +2,18 @@ package com.tangpj.screenadaptation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvSw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int sw = getResources().getConfiguration().smallestScreenWidthDp;
+        tvSw = findViewById(R.id.tv_sw);
+        tvSw.setText("sw = " + sw + "dp");
     }
 }
